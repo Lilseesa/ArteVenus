769716fa4912b52243cd9e72c129bb0b25b79daa
Sven <svenebal@gmail.com>
2021-05-22 14:22:36 -0600

    Arreglo del footer

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /* Inline script printed out in the header */
 add_action('wp_enqueue_scripts', 'add_script');
 function add_script() {
-    wp_register_script('js-home', get_stylesheet_directory_uri().'/js/main.js', array(), '1', true);
+    wp_register_script('js-home', get_stylesheet_directory_uri().'js/main.js', array(), '1', true);
 	wp_enqueue_script('js-home');
 	wp_register_script('js-swiper', 'https://unpkg.com/swiper/swiper-bundle.js', array(), '1', true);
 	wp_enqueue_script('js-swiper');
