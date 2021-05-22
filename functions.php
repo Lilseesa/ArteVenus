8dcb32f967feb71e68f64b4185d160c05689f88b
Sven <svenebal@gmail.com>
2021-05-22 14:32:20 -0600

    Arreglo del functions

--- functions.php
+++ functions.php
@@ -4,8 +4,8 @@ add_action('wp_enqueue_scripts', 'add_script');
 function add_script() {
     wp_register_script('js-home', get_stylesheet_directory_uri().'js/main.js', array(), '1', true);
 	wp_enqueue_script('js-home');
-	wp_register_script('js-swiper', 'https://unpkg.com/swiper/swiper-bundle.js', array(), '1', true);
+	wp_register_script('js-swiper', 'https://unpkg.com/swiper/swiper-bundle.js', array(), '6.6.2', true);
 	wp_enqueue_script('js-swiper');
-	wp_register_script('swiper', 'https://unpkg.com/swiper/swiper-bundle.min.js', array(), '1', true);
+	wp_register_script('swiper', 'https://unpkg.com/swiper/swiper-bundle.min.js', array(), '6.6.2', true);
 	wp_enqueue_script('swiper');
 }
