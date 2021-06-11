0f2948cbc39b51a5283769f7e5a351b20587cfce
Sven <svenebal@gmail.com>
2021-06-11 17:00:26 -0600

    About responsive

--- page-about-us.php
+++ page-about-us.php
@@ -18,32 +18,42 @@ while( have_posts() ) : the_post(); ?>
 
     <body>
 
-        <div class="container">
-            <div class="abt_content">
-                <h1 class="abt_elements abt_mainTitle">About</h1>
-                <h2 class="abt_elements abt_mainSubtitle">us</h2>
-            </div>    
+    <div class="container">
+
+        <div class="row abt_main_titles">
+            <h1 class="abt_elements abt_mainTitle subtitle">About</h1>
+            <h2 class="abt_elements abt_mainSubtitle subtitle">us</h2>
         </div>
 
-        <div class="abt_containerHeader">
-            <h1 class="abt_elements abt_title">Our</h1>
-            <h2 class="abt_elements abt_subtitle">history</h2>  
+        <div class="row mt-5 abt_main_content hm_boder-radius-section" >
+            <div class="row">
+                <!-- Contact us Title -->
+
+                <div class="abt_containerHeader">
+                    <h1 class="abt_elements abt_title title">Our</h1>
+                    <h2 class="abt_elements abt_subtitle title">history</h2>    
+                </div>
+
+            </div>
         </div>
 
-        <div class="abt_containerText">
-            <!--<?php the_content(); ?>-->
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div class="abt_secondary_content hm_boder-radius-section" >
+            <div class="row">
+                <!-- info -->
+                <div class="abt_containerText card-price-desktop">
+                    <!--<?php the_content(); ?>-->
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
         </div>
 
-        <div class="text-center">
-            <img class="img-fluid" src="<?php echo get_template_directory_uri().'/src/images/Trapezoid_About.svg';?>" alt="khee"> 
-        </div>
+    </div>
 
-        <div class="container abt_image">
-            <img class="img-fluid" src="<?php echo get_template_directory_uri().'/src/images/Trapezoid_Team_Image.png';?>" alt="kheex2"> 
-        </div>
-
-        <?php endwhile; ?>
+    <?php endwhile; ?>
     </body>
+
+    <?php
+        get_footer();
+    ?>
