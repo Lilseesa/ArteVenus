dc5cfc39dc0a90ce8217efdc040e4c5e4e29bcf2
Makkpas <axisfs12@gmail.com>
2021-06-30 00:49:20 -0600

    Single Product v2

--- page-single-product.php
+++ page-single-product.php
@@ -23,20 +23,33 @@ while( have_posts() ) : the_post(); ?>
             <div class="col-md-6 mb-2"> 
                 <img src="http://smallbiztrends.com/wp-content/uploads/2018/10/shutterstock_354254963.jpg" class="img-fluid border-image-radius" alt="...">
             </div>
-            <div class="col-md-6"> 
-                <h1 class="single_prod_title">Products Name</h1>
-                <h4>$199.99</h4>
-                <h5>Styles</h5>
-                <div class="row">
-                    <!-- TODO: REVISAR QUE LAS NUEVAS CLASES SE ESTÉN PROCESANDO EN WORDPRESS -->
-                    <img src="http://smallbiztrends.com/wp-content/uploads/2018/10/shutterstock_354254963.jpg" class="small-product-image" alt="...">
-                    <img src="http://smallbiztrends.com/wp-content/uploads/2018/10/shutterstock_354254963.jpg" class="small-product-image" alt="...">
-                    <img src="http://smallbiztrends.com/wp-content/uploads/2018/10/shutterstock_354254963.jpg" class="small-product-image" alt="...">
+                <div class="col-md-6"> 
+                    <h1 class="single_prod_title">Products Name</h1>
+                    <h4 class="single-prod-price">$199.99</h4>
+                        <h5 class="single-prod-section-title">Styles:</h5>
+                        <div class="row">
+                            <div class="d-flex flex-wrap">
+                                <!-- TODO: REVISAR QUE LAS NUEVAS CLASES SE ESTÉN PROCESANDO EN WORDPRESS -->
+                                <img src="http://smallbiztrends.com/wp-content/uploads/2018/10/shutterstock_354254963.jpg" class="small-product-image mx-1" alt="...">
+                                <img src="http://smallbiztrends.com/wp-content/uploads/2018/10/shutterstock_354254963.jpg" class="small-product-image mx-1" alt="...">
+                                <img src="http://smallbiztrends.com/wp-content/uploads/2018/10/shutterstock_354254963.jpg" class="small-product-image mx-1" alt="...">
+                            </div>
+                            <div class="position-relative my-3">
+                                <input class="single-prod-quantity-input" type="number" name="" id="" value="1" min="1">
+                            </div>
+                            
+                        </div>
+                        <p class="single-prod-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="single-prod-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <div class="position-relative my-5">
+                            <button type="button" class="btn-buy-products button-rounded-filled-large text-center position-absolute top-0 start-50 translate-middle">Buy Now</button>
+                        </div>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
         </div>
+
+        
+        <!--Products Section -->
         <div class="row p-3 mt-3">
             <div class="col-lg-12">
                 <!--Products Title -->
@@ -48,7 +61,7 @@ while( have_posts() ) : the_post(); ?>
                             <div class="d-flex flex-column pt-2">
                                 <p class="card-info-text-v2 mx-3">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                                 <div class="d-flex mx-auto">
-                                    <button type="button" class="btn-buy button-rounded-filled">Buy Now</button>
+                                    <button type="button" class="btn-buy-products button-rounded-filledv2">Buy Now</button>
                                 </div>
                             </div>
                         </div>
@@ -61,7 +74,7 @@ while( have_posts() ) : the_post(); ?>
                             <div class="d-flex flex-column pt-2">
                                 <p class="card-info-text-v2 mx-3">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                                 <div class="d-flex mx-auto">
-                                    <button type="button" class="btn-buy button-rounded-filled">Buy Now</button>
+                                    <button type="button" class="btn-buy-products button-rounded-filledv2">Buy Now</button>
                                 </div>
                             </div>
                         </div>
@@ -74,7 +87,7 @@ while( have_posts() ) : the_post(); ?>
                             <div class="d-flex flex-column pt-2">
                                 <p class="card-info-text-v2 mx-3">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                                 <div class="d-flex mx-auto">
-                                    <button type="button" class="btn-buy button-rounded-filled">Buy Now </button>
+                                    <button type="button" class="btn-buy-products button-rounded-filledv2">Buy Now</button>
                                 </div>
                             </div>
                         </div>
